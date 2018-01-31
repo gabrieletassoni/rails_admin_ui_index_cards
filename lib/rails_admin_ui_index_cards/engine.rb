8837adc53eb5a314e438c0357d1c3b30fa5db707
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2018-01-31 22:41:03 +0100

    Added static assets manager

--- lib/rails_admin_ui_index_cards/engine.rb
+++ lib/rails_admin_ui_index_cards/engine.rb
@@ -1,4 +1,11 @@
 module RailsAdminUiIndexCards
   class Engine < ::Rails::Engine
+    # This code makes the static assets present in this 
+    # engine available to main app, even if this gem is 
+    # just a dependency of another engine
+    initializer 'rails_admin_ui_index_cards.load_static_assets' do |app|
+      puts "Loading static assets for #{root}"
+      app.middleware.use ::ActionDispatch::Static, "#{root}/app"
+    end
   end
 end
